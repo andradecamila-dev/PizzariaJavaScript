b0bcdda86187bbbf6d21fd5cd6bfb0021061afea
Camila Andrade <113257108+andradecamila-dev@users.noreply.github.com>
2024-06-16 20:26:48 -0300

    cart

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 let modalQt = 1;
+let cart = []; //array
+let modalKey = 0;
+
 const c = (el)=>document.querySelector(el);
 const cs = (el)=>document.querySelectorAll(el);
 
@@ -14,6 +17,7 @@ pizzaJson.map((item, index)=>{
         e.preventDefault();
         let key = e.target.closest('.pizza-item').getAttribute('data-key');
         modalQt = 1;
+        modalKey = key;  //vai mostrar qual a pizz
 
         c('.pizzaBig img ').src = pizzaJson[key].img;
         c('.pizzaInfo h1').innerHTML = pizzaJson[key].name;
@@ -56,5 +60,65 @@ pizzaJson.map((item, index)=>{
         item.addEventListener('click', closemodal);
 
     } );
+    
+    //eventos de quantidade
+    
+    c('.pizzaInfo--qtmenos').addEventListener('click', ()=>{
+        if(modalQt > 1){
+            modalQt--;
+            c('.pizzaInfo--qt').innerHTML = modalQt;
 
+        }
+    });
+
+    c('.pizzaInfo--qtmais').addEventListener('click', ()=>{
+       modalQt++;
+       c('.pizzaInfo--qt').innerHTML = modalQt;
+    });
+
+    cs('.pizzaInfo--size').forEach((size, sizeIndex)=>{
+        size.addEventListener('click', (e)=>{
+            c('.pizzaInfo--size.selected').classList.remove('selected');
+            size.classList.add('selected');
+        });
+
+    });
+
+    c('.pizzaInfo--addButton').addEventListener('click', ()=>{
+        //info qnt, tamanho, e sabor
+        let size = c('.pizzaInfo--size.selected').getAttribute('data-key'); //data-key é o atributo de tamanho do html
+
+        let identifier = pizzaJson[modalKey].id+'@'+size;  
+
+        let key = cart.findIndex((item)=>item.identifier == identifier);
+        
+            if ( key > -1){
+                cart[key].qt += modalQt;
+            }
+            else{
+            cart.push({
+                identifier,
+                id:pizzaJson[modalKey]
+                .id,
+                size,
+                qt:modalQt
+             });
+            }  
+        updateCart();     
+        closemodal();
+});
+
+        function updateCart(){
+            if (cart.length > 0) {
+               c('aside').classList.add('show');
+                for(let i in cart) {
+                    let pizzaItem = pizzaJson.find((item)=> item.id == cart[i].id);
+
+                console.log(pizzaItem);
+            } 
+        }
+        else {
+                c('aside').classList.remove('show');
+            }
+        }
     
