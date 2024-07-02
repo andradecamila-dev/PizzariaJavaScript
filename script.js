bc9d4ef572c91554c39557d04e94de43b1064edc
Camila Andrade <113257108+andradecamila-dev@users.noreply.github.com>
2024-07-02 20:41:18 -0300

    .ok

--- script.js
+++ script.js
@@ -2,6 +2,19 @@ let modalQt = 1;
 let cart = []; //array
 let modalKey = 0;
 
+function toggleModel(){
+  const html = document.documentElement
+
+  if (body.classList.contains('light')){
+html.classList.remove('light')
+
+  }
+    else {
+        html.classList.add('light')
+    }
+
+}
+
 const c = (el)=>document.querySelector(el);
 const cs = (el)=>document.querySelectorAll(el);
 
